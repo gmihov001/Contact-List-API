a17cbd749e5c5cc2b4e07fb188b5385b363c6e35
George Mihov <gmihov001@gmail.com>
2021-01-22 17:44:57 +0000

    updates

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Contact
 #from models import Person
 
 app = Flask(__name__)
@@ -31,13 +31,10 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+def get_all_contacts():
+    contacts = Contact.query.all()
+    contacts = list(map(lambda x:x.serialize(), contacts))
+    return jsonify(contacts), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
